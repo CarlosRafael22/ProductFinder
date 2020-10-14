ca2e4cac6bed497b49a5a24ca609b9606c2ba0d8
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-10-14 13:45:44 -0300

    feat: Add views and url to search products and show those saved on json

--- finder/views.py
+++ finder/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from typing import List
 import sys
 sys.path.insert(1, 'C:\\Users\\carlo\\Documents\\ESTUDOS\\Web Scraping\\Furniture Scraping\\')
@@ -8,7 +9,7 @@ from products import Product
 
 def get_products_from_query(query: str) -> List[dict]:
     ''' Returns a list of dictionaries with the products queried with the DataRetriever query_for method '''
-    retriever = DataRetriever(['magazineluiza', 'submarino'])
+    retriever = DataRetriever(['americanas', 'submarino'])
     products_dicts = retriever.query_for(query)
     return products_dicts
 
@@ -26,18 +27,29 @@ def retrieve_products_from_json(file_name: str) -> List[Product]:
 
 # Create your views here.
 def render_main(request):
-    from extractor import PageExtractor, DataRetriever
-    # import pdb; pdb.set_trace()
-    page = PageExtractor('magazineluiza')
-    products_dicts = page.query_webdriver('cadeira gamer')
+    # from extractor import PageExtractor, DataRetriever
+    # # import pdb; pdb.set_trace()
+    # page = PageExtractor('magazineluiza')
+    # products_dicts = page.query_webdriver('cadeira gamer')
 
-    page = PageExtractor('submarino')
-    products_dicts2 = page.query_webdriver('cadeira gamer')
+    # page = PageExtractor('submarino')
+    # products_dicts2 = page.query_webdriver('cadeira gamer')
 
-    selected_products = products_dicts[:10] + products_dicts2[:10]
+    # selected_products = products_dicts[:10] + products_dicts2[:10]
     # page.store_products_on_json(selected_products, 'products.json')
 
     # selected_products = DataRetriever.get_products_from_json('products.json')
+    products = retrieve_products_from_json('search_products.json')
 
-    context = {'products': selected_products}
+    context = {'products': products, 'products_total': len(products)}
     return render(request, 'finder/main.html', context)
+
+
+def find_products_with_query(request, query_word):
+    parsed_query = query_word.replace('-', ' ')
+    products_dicts = get_products_from_query(parsed_query)
+
+    # Saving on JSON for the template to show data from this
+    save_products_on_json(products_dicts, 'search_products.json')
+    # import pdb; pdb.set_trace()
+    return JsonResponse({'products': products_dicts})
