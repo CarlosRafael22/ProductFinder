38a1bab67f60cf0bee23de213a084115e4c48f3d
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-10-14 12:38:28 -0300

    feat: Add query method with DataRetriever, save/retrieve from json and its tests

---
+++ finder/tests/test_views.py
@@ -0,0 +1,28 @@
+from finder.views import *
+import pytest
+
+class TestViews:
+    def test_should_get_products_from_query(self):
+        import os
+        products_dicts = get_products_from_query('cadeira gamer')
+        assert type(products_dicts[0]) == dict
+
+        file_name = 'test.json'
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        save_products_on_json(products_dicts, file_name)
+        assert os.path.exists(file_name) == True, 'Should have saved products dicts on json'
+
+    def test_should_retrieve_products_from_json(self):
+        import os
+        file_name = 'test.json'
+        if os.path.exists(file_name):
+            products = DataRetriever.get_products_from_json(file_name)
+            assert products[0] != None
+        else:
+            with pytest.raises(Exception):
+                products = DataRetriever.get_products_from_json(file_name)
+
+
+        
+
--- finder/views.py
+++ finder/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from typing import List
 import sys
-sys.path.insert(1, 'C:\\Users\\carlo\\Documents\\ESTUDOS\\WebScraping\\FurnitureScraping\\')
-# from ../../../WebScraping.FurnitureScraping.extractor import PageExtractor
+sys.path.insert(1, 'C:\\Users\\carlo\\Documents\\ESTUDOS\\Web Scraping\\Furniture Scraping\\')
+from extractor import PageExtractor, DataRetriever
+from products import Product
+
+
+def get_products_from_query(query: str) -> List[dict]:
+    ''' Returns a list of dictionaries with the products queried with the DataRetriever query_for method '''
+    retriever = DataRetriever(['magazineluiza', 'submarino'])
+    products_dicts = retriever.query_for(query)
+    return products_dicts
+
+
+def save_products_on_json(products_dicts: List[dict], file_name: str):
+    ''' Saves the dictionaries of products on json file '''
+    DataRetriever.store_products_on_json(products_dicts, file_name)
+
+
+def retrieve_products_from_json(products_dicts: List[dict], file_name: str) -> List[Product]:
+    ''' Loads list of products from json file '''
+    products = DataRetriever.get_products_from_json(products_dicts, file_name)
+    return products
+
 
 # Create your views here.
 def render_main(request):
