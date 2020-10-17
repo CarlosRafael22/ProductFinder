b7c2051de3065b07ec02b7c94821791ba8fcf949
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-10-17 19:44:47 -0300

    feat(settings.py): Change database to Postgres

--- productfinder/settings.py
+++ productfinder/settings.py
@@ -74,11 +74,22 @@ WSGI_APPLICATION = 'productfinder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'productfinder',
+      'USER':'postgres',
+      'PASSWORD':'admin',
+      'HOST':'localhost',
+      'PORT':'',
+   }
 }
 
 
