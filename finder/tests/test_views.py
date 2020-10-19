00521bdf41195d3ba87bfea8530a9ea894c59be9
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-10-19 19:52:42 -0300

    test(test_views): Add test for ShowProduct view

--- finder/tests/test_views.py
+++ finder/tests/test_views.py
@@ -29,3 +29,34 @@ class TestSearchProductsWithQueryView:
             assert database_products.count() > previous_database_total
             # Since when creating products on database it checks whether its been already inserted the total might not be the same
             assert len(products_dicts) >= database_products.count()
+
+# CANT USE query string params on reverse so found this modification on the internet
+# https://gist.github.com/benbacardi/227f924ec1d9bedd242b
+def reverse_querystring(view, urlconf=None, args=None, kwargs=None, current_app=None, query_kwargs=None):
+    '''Custom reverse to handle query strings.
+    Usage:
+        reverse('app.views.my_view', kwargs={'pk': 123}, query_kwargs={'search': 'Bob'})
+    '''
+    from django.utils.http import urlencode
+
+    base_url = reverse(view, urlconf=urlconf, args=args, kwargs=kwargs, current_app=current_app)
+    if query_kwargs:
+        return '{}?{}'.format(base_url, urlencode(query_kwargs))
+    return base_url
+
+class TestShowProductsStoredOnDatabaseView:
+    def test_should_show_products(self, client):
+        from finder.services import retrieve_products_from_json, create_product_model_from_dict
+        import json
+
+        # Putting test products on database
+        products_objects = retrieve_products_from_json('test_show_products.json')
+        previous_database_total = Product.objects.count()
+        for prod in products_objects:
+            create_product_model_from_dict(prod.__dict__)
+        current_database_total = Product.objects.count()
+        assert previous_database_total != current_database_total
+
+        show_url = reverse_querystring('finder-show', query_kwargs={'price__gte': 70})
+        response = client.get(show_url)
+        assert response.status_code == 200
