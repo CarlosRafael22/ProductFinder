7d9a9f449179721250865e68d6ba9eee4265444d
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-10-21 17:47:22 -0300

    feat(settings): Add django-cors to requirements and created cors whitelist

--- productfinder/settings.py
+++ productfinder/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
+
 
 # Application definition
 
@@ -41,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
